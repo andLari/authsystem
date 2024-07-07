4d2e50ecda1970d1cb9921b60eb6adc4b231e688
andLari <69187209+andLari@users.noreply.github.com>
2024-07-07 13:43:20 +0300

    Update databaseconfig.cs

--- databaseconfig.cs
+++ databaseconfig.cs
@@ -8,6 +8,6 @@ namespace variant3compservice
 {
     public static class databaseconfig
     {
-        public static string sqlstring = @"Data Source = 192.168.4.248; Initial Catalog = db10; Integrated Security = True";
+        public static string sqlstring = @"Data Source = your address; Initial Catalog = db10; Integrated Security = True";
     }
 }
